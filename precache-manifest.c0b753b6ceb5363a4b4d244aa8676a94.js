d3daf8660e0fba3cd3c9e75ff16ff725b665893f
AS_Baranov <artem765@inbox.ru>
2023-09-14 16:02:58 +0300

    Updates

---
+++ precache-manifest.c0b753b6ceb5363a4b4d244aa8676a94.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ee7113915a7ade01f89",
+    "url": "/mod6/static/css/main.74caf85a.chunk.css"
+  },
+  {
+    "revision": "0ee7113915a7ade01f89",
+    "url": "/mod6/static/js/main.69661199.chunk.js"
+  },
+  {
+    "revision": "c20e21d6e6cf5c7075b1",
+    "url": "/mod6/static/js/runtime~main.1f93892b.js"
+  },
+  {
+    "revision": "6ea2ea22c9449bf5e553",
+    "url": "/mod6/static/js/2.5258f49a.chunk.js"
+  },
+  {
+    "revision": "03c2247ebef71b42a125",
+    "url": "/mod6/static/js/3.133d7e0c.chunk.js"
+  },
+  {
+    "revision": "542dfa0368177543432288380e0c19bb",
+    "url": "/mod6/static/media/1.542dfa03.png"
+  },
+  {
+    "revision": "e8b583a8960fccd3b9c01c1680898873",
+    "url": "/mod6/static/media/2.e8b583a8.png"
+  },
+  {
+    "revision": "1a3b5fcb10660bee642f57d4fbeb2367",
+    "url": "/mod6/static/media/3.1a3b5fcb.png"
+  },
+  {
+    "revision": "2cbb2ab408e9affe855e731aa8053aed",
+    "url": "/mod6/static/media/4.2cbb2ab4.png"
+  },
+  {
+    "revision": "8ad326d6f6514e19fa294c6f9f7bfbbe",
+    "url": "/mod6/static/media/5.8ad326d6.png"
+  },
+  {
+    "revision": "b0d4f8ed9e174468001cfb97be99476a",
+    "url": "/mod6/static/media/6.b0d4f8ed.png"
+  },
+  {
+    "revision": "b3ba703c591edd4aad57f8f4561a287b",
+    "url": "/mod6/static/media/Montserrat-Medium.b3ba703c.ttf"
+  },
+  {
+    "revision": "3fe868a1a9930b59d94d2c1d79461e3c",
+    "url": "/mod6/static/media/Montserrat-Regular.3fe868a1.ttf"
+  },
+  {
+    "revision": "0dc7ccd81c27e2fca57bebda54e11e09",
+    "url": "/mod6/static/media/JetBrainsMonoNL-Regular.0dc7ccd8.ttf"
+  },
+  {
+    "revision": "1413b54ff1805eaf9b560f7487339160",
+    "url": "/mod6/index.html"
+  }
+];
